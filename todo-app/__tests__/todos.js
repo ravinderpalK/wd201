e0ca00fcee223b0f5657e5db0c653d6e9066e9ce
ravinderpal <ravinderpalsinghkhokhar@gmail.com>
2023-05-01 12:11:32 +0530

    updated setCompletionStatus route

--- todo-app/__tests__/todos.js
+++ todo-app/__tests__/todos.js
@@ -60,6 +60,7 @@ describe("Todo Application", function () {
       .put(`/todos/${latestTodo.id}`)
       .send({
         _csrf: csrfToken,
+        completed: latestTodo.completed,
       });
     const parsedUpdatedResponse = JSON.parse(completionStatusResponse.text);
     expect(parsedUpdatedResponse.completed).toBe(true);
@@ -90,6 +91,7 @@ describe("Todo Application", function () {
       .put(`/todos/${latestTodo.id}`)
       .send({
         _csrf: csrfToken,
+        completed: latestTodo.completed,
       });
     const parsedUpdatedResponse = JSON.parse(completionStatusResponse.text);
     expect(parsedUpdatedResponse.completed).toBe(false);
