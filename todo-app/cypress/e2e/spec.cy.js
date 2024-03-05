02cce143b74df408ac00de009da26905b08f5124
ravinderpal <ravinderpalsinghkhokhar@gmail.com>
2024-03-05 22:50:58 +0530

    added docker build and push

--- todo-app/cypress/e2e/spec.cy.js
+++ todo-app/cypress/e2e/spec.cy.js
@@ -61,7 +61,7 @@ describe("Todo App Test,", () => {
     cy.get('button[type="submit"]').click();
     cy.wait(500);
     cy.get(".todo-item").should("exist");
-    cy.get("#count-duetoday").contains("3");
+    cy.get("#count-duetoday").contains("1");
   });
 
   it("Delete Todo", () => {
